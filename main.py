12cc777d035da0cde832d317024f5f2623895340
Agustin <agustinbaied1@gmail.com>
2025-12-08 22:36:53 -0300

    base to work

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Administrador de Inventario de Tienda (Basado en Clases)
+
+Objetivo: Crear un programa interactivo que simule la gestión de productos en una pequeña tienda.
+
+Conocimientos a aplicar:
+
+Clases: Definir clases, encapsulando la lógica.
+
+Usar un diccionario dentro de la clase Tienda para guardar los productos (Clave: Nombre del producto, Valor: Cantidad disponible o Precio).
+
+Crear un menú interactivo CRUD
+
+Usar anidamiento de funciones
+"""
